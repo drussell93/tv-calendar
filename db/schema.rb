3c551c33a125aee781be6a6b1fe2060581aba8d5
Gillian Conley <gconley@uccs.edu>
2017-10-25 05:09:24 +0000

    Added a basic database table and seed -Gillian

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171024234455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shows", force: :cascade do |t|
+    t.string "title"
+    t.string "start_time"
+    t.string "end_time"
+    t.string "date"
+    t.string "description"
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+shows = [{:title => 'The Flash', :start_time =>'1400', :end_time =>'1500', :date => '10.24.2017', :description =>'Availible: Netflix, and nothing else matters, right?'},
+         {:title => 'The Meaning of Life', :start_time => '1600', :end_time => '1800', :date => '10.1.2017', :description =>'Monty Python.  You know the one'},
+         {:title => 'Lucifer', :start_time => '1500', :end_time => '1700', :date => '10.23.2017', :description =>'Availible Mondays, TNT, I think?'},
+         {:title => 'Spirited Away', :start_time => '0900', :end_time => '1100',:date => '12.5.2017', :description =>'Movie by Miyazaki'},
+         {:title => 'A Series of Unfortunate Events', :start_time =>'0000', :end_time =>'2359', :date => '2.1.2018', :description =>'Season 2, Availible: Netflix'}
+        ]
+        
+show.each do |show|
+    Show.create!(show)
+end
