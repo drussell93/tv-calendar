f981e5d5202577ff479f6fa3b891d3e0fe1d1b39
Kevin Yu <kyu2@uccs.edu>
2017-10-26 20:01:20 +0000

    editted the seed because it wasn't correct.

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024234455) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "shows", force: :cascade do |t|
-    t.string "title"
-    t.string "start_time"
-    t.string "end_time"
-    t.string "date"
-    t.string "description"
-  end
 
 end
